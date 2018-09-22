2c8687daea80fb4e20916497ad88609014fe423e
Alex Richards <arichards@netjets.com>
2018-09-22 16:29:41 -0400

    Abstracted driver creation out of trip tests

--- spec/DriveTrackerSpec.js
+++ spec/DriveTrackerSpec.js
@@ -10,17 +10,21 @@ describe("The Driver", function() {
   });
 });
 describe("The Trip", function(){
+    const Alex = new Driver();
   it("should be a trip", function(){
     const testDrive = new Trip();
     expect(testDrive instanceof Trip).toBeTruthy();
   });
   it("should have a driver", function(){
-    const Alex = new Driver();
     const testDrive = new Trip(Alex);
     expect(testDrive.driver).toBe(Alex);
   });
   it("should have a start time", function(){
-    const Alex = new Driver();
+    const startTime = "6:00";
+    const testDrive = new Trip(Alex, startTime);
+    expect(testDrive.startTime).toBe(startTime);
+  });
+  it("should have a start time", function(){
     const startTime = "6:00";
     const testDrive = new Trip(Alex, startTime);
     expect(testDrive.startTime).toBe(startTime);
